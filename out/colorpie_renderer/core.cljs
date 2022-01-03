16c0155a9906310a9cadbc2418e7924d7750bd6e
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-03 18:31:16 +0900

    output js-file.

--- out/colorpie_renderer/core.cljs
+++ out/colorpie_renderer/core.cljs
@@ -1,3 +1,9 @@
 (ns colorpie-renderer.core)
 
-(println "Hello world!")
+(def canvas (.getElementById js/document "canvas"))
+(def ctx (.getContext canvas "2d"))
+
+(set! (.-fillStyle ctx) "rgb(200,200,20)")
+(.beginPath ctx)
+(.rect ctx 100 100 50 30)
+(.fill ctx)
