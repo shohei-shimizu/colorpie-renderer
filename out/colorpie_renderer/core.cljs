4b314434700b4c1398891c3de40b20c8b53f7d16
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-03 18:33:57 +0900

    recompile.

--- out/colorpie_renderer/core.cljs
+++ out/colorpie_renderer/core.cljs
@@ -3,7 +3,7 @@
 (def canvas (.getElementById js/document "canvas"))
 (def ctx (.getContext canvas "2d"))
 
-(set! (.-fillStyle ctx) "rgb(200,200,20)")
+(set! (.-fillStyle ctx) "rgb(255,0,0)")
 (.beginPath ctx)
 (.rect ctx 100 100 50 30)
 (.fill ctx)
--- out/colorpie_renderer/core.js
+++ out/colorpie_renderer/core.js
@@ -3,7 +3,7 @@ goog.provide('colorpie_renderer.core');
 goog.require('cljs.core');
 colorpie_renderer.core.canvas = document.getElementById("canvas");
 colorpie_renderer.core.ctx = colorpie_renderer.core.canvas.getContext("2d");
-(colorpie_renderer.core.ctx.fillStyle = "rgb(200,200,20)");
+(colorpie_renderer.core.ctx.fillStyle = "rgb(255,0,0)");
 colorpie_renderer.core.ctx.beginPath();
 colorpie_renderer.core.ctx.rect((100),(100),(50),(30));
 colorpie_renderer.core.ctx.fill();
