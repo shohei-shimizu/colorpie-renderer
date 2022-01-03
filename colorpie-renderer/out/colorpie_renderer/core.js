64d4ce7af8c474bd5beedd6f93618291f4c93fed
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-03 13:58:09 +0900

    make test source and compile that.

---
+++ colorpie-renderer/out/colorpie_renderer/core.js
@@ -0,0 +1,6 @@
+// Compiled by ClojureScript 1.10.758 {}
+goog.provide('colorpie_renderer.core');
+goog.require('cljs.core');
+cljs.core.println.call(null,"Hello world!");
+
+//# sourceMappingURL=core.js.map
