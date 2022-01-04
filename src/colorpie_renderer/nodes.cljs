3eae29b4721eefcfc36bc86700b542eab3f13243
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-05 01:13:14 +0900

    remove center record.

--- src/colorpie_renderer/nodes.cljs
+++ src/colorpie_renderer/nodes.cljs
@@ -8,9 +8,6 @@
 ;; Intersection-nodes are composition for double Nodes.
 (defrecord Intersection-nodes [name colors coordinate])
 
-;; Center-nodes are special Intersection-nodes that are opposition of each Nodes.
-(defrecord Center-nodes [name colors coordinate])
-
 ;; ---- Nodes instances ----
 ;; Define each 10 nodes about name, colors, and coordinate.
 (def consideration
