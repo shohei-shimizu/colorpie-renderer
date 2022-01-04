ac1d4fbb37231da806c6509e84343d78df5e1f2b
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-04 19:25:15 +0900

    reset main branch sorry.

--- out/colorpie_renderer/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns colorpie-renderer.core)
-
-(def canvas (.getElementById js/document "canvas"))
-(def ctx (.getContext canvas "2d"))
-
-(set! (.-fillStyle ctx) "rgb(255,0,0)")
-(.beginPath ctx)
-(.rect ctx 100 100 50 30)
-(.fill ctx)
