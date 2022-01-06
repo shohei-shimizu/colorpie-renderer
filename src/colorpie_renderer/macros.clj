0fd6fb7b30723f705f3b7ca7188561d65e88210f
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-06 14:48:14 +0900

    rename file

--- src/colorpie_renderer/macros.clj
+++
@@ -1,7 +0,0 @@
-(ns colorpie-renderer.macros)
-(defmacro def-nodes [x y z]
- `(do
-    (def ~x (~y ~z))
-    ~z))
-
-(macroexpand-1 '(def-nodes consideration map->Nodes {:name "配慮" :colors [::white] :interrogative ::who}))
