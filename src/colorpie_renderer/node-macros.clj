c6fb3e705dddbf59de77f63784645b490a76607e
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-06 17:27:19 +0900

    rename

--- src/colorpie_renderer/node-macros.clj
+++
@@ -1,5 +0,0 @@
-(ns colorpie-renderer.macros)
-(defmacro def-nodes [x y z]
- `(do
-    (def ~x (~y ~z))
-    ~z))
--- src/colorpie_renderer/sets-macros.clj
+++
@@ -1,5 +0,0 @@
-(ns colorpie-renderer.sets-macros)
-
-(defmacro def-sets [x y z]
-  `(do (def ~x (~y ~z))
-       ~z))
