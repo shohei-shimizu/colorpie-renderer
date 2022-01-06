a8ba9639b86dc1a46c809f31ab602e2dcc0d9958
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-06 15:29:18 +0900

    add macro and intersection-sets

--- out/cider/nrepl/inlined_deps/suitable/v0v4v0/suitable/js_introspection.js
+++ out/cider/nrepl/inlined_deps/suitable/v0v4v0/suitable/js_introspection.js
@@ -18,10 +18,10 @@ var obj__$1 = obj;
 var protos = cljs.core.PersistentVector.EMPTY;
 while(true){
 if(cljs.core.truth_(obj__$1)){
-var G__12163 = Object.getPrototypeOf(obj__$1);
-var G__12164 = cljs.core.conj.call(null,protos,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"obj","obj",981763962),obj__$1,new cljs.core.Keyword(null,"props","props",453281727),cider.nrepl.inlined_deps.suitable.v0v4v0.suitable.js_introspection.own_property_descriptors.call(null,obj__$1)], null));
-obj__$1 = G__12163;
-protos = G__12164;
+var G__10493 = Object.getPrototypeOf(obj__$1);
+var G__10494 = cljs.core.conj.call(null,protos,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"obj","obj",981763962),obj__$1,new cljs.core.Keyword(null,"props","props",453281727),cider.nrepl.inlined_deps.suitable.v0v4v0.suitable.js_introspection.own_property_descriptors.call(null,obj__$1)], null));
+obj__$1 = G__10493;
+protos = G__10494;
 continue;
 } else {
 return protos;
@@ -30,8 +30,8 @@ break;
 }
 });
 cider.nrepl.inlined_deps.suitable.v0v4v0.suitable.js_introspection.property_names_and_types = (function cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$property_names_and_types(var_args){
-var G__12166 = arguments.length;
-switch (G__12166) {
+var G__10496 = arguments.length;
+switch (G__10496) {
 case 1:
 return cider.nrepl.inlined_deps.suitable.v0v4v0.suitable.js_introspection.property_names_and_types.cljs$core$IFn$_invoke$arity$1((arguments[(0)]));
 
@@ -52,38 +52,38 @@ return cider.nrepl.inlined_deps.suitable.v0v4v0.suitable.js_introspection.proper
 
 (cider.nrepl.inlined_deps.suitable.v0v4v0.suitable.js_introspection.property_names_and_types.cljs$core$IFn$_invoke$arity$2 = (function (js_obj,prefix){
 var seen = cljs.core.transient$.call(null,cljs.core.PersistentHashSet.EMPTY);
-var iter__4529__auto__ = (function cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__12168(s__12169){
+var iter__4529__auto__ = (function cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__10497(s__10498){
 return (new cljs.core.LazySeq(null,(function (){
-var s__12169__$1 = s__12169;
+var s__10498__$1 = s__10498;
 while(true){
-var temp__5753__auto__ = cljs.core.seq.call(null,s__12169__$1);
+var temp__5753__auto__ = cljs.core.seq.call(null,s__10498__$1);
 if(temp__5753__auto__){
 var xs__6308__auto__ = temp__5753__auto__;
-var vec__12174 = cljs.core.first.call(null,xs__6308__auto__);
-var i = cljs.core.nth.call(null,vec__12174,(0),null);
-var map__12177 = cljs.core.nth.call(null,vec__12174,(1),null);
-var map__12177__$1 = (((((!((map__12177 == null))))?(((((map__12177.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__12177.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.call(null,cljs.core.hash_map,map__12177):map__12177);
-var obj = cljs.core.get.call(null,map__12177__$1,new cljs.core.Keyword(null,"obj","obj",981763962));
-var props = cljs.core.get.call(null,map__12177__$1,new cljs.core.Keyword(null,"props","props",453281727));
-var iterys__4525__auto__ = ((function (s__12169__$1,vec__12174,i,map__12177,map__12177__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen){
-return (function cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__12168_$_iter__12170(s__12171){
-return (new cljs.core.LazySeq(null,((function (s__12169__$1,vec__12174,i,map__12177,map__12177__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen){
+var vec__10503 = cljs.core.first.call(null,xs__6308__auto__);
+var i = cljs.core.nth.call(null,vec__10503,(0),null);
+var map__10506 = cljs.core.nth.call(null,vec__10503,(1),null);
+var map__10506__$1 = (((((!((map__10506 == null))))?(((((map__10506.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__10506.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.call(null,cljs.core.hash_map,map__10506):map__10506);
+var obj = cljs.core.get.call(null,map__10506__$1,new cljs.core.Keyword(null,"obj","obj",981763962));
+var props = cljs.core.get.call(null,map__10506__$1,new cljs.core.Keyword(null,"props","props",453281727));
+var iterys__4525__auto__ = ((function (s__10498__$1,vec__10503,i,map__10506,map__10506__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen){
+return (function cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__10497_$_iter__10499(s__10500){
+return (new cljs.core.LazySeq(null,((function (s__10498__$1,vec__10503,i,map__10506,map__10506__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen){
 return (function (){
-var s__12171__$1 = s__12171;
+var s__10500__$1 = s__10500;
 while(true){
-var temp__5753__auto____$1 = cljs.core.seq.call(null,s__12171__$1);
+var temp__5753__auto____$1 = cljs.core.seq.call(null,s__10500__$1);
 if(temp__5753__auto____$1){
-var s__12171__$2 = temp__5753__auto____$1;
-if(cljs.core.chunked_seq_QMARK_.call(null,s__12171__$2)){
-var c__4527__auto__ = cljs.core.chunk_first.call(null,s__12171__$2);
+var s__10500__$2 = temp__5753__auto____$1;
+if(cljs.core.chunked_seq_QMARK_.call(null,s__10500__$2)){
+var c__4527__auto__ = cljs.core.chunk_first.call(null,s__10500__$2);
 var size__4528__auto__ = cljs.core.count.call(null,c__4527__auto__);
-var b__12173 = cljs.core.chunk_buffer.call(null,size__4528__auto__);
-if((function (){var i__12172 = (0);
+var b__10502 = cljs.core.chunk_buffer.call(null,size__4528__auto__);
+if((function (){var i__10501 = (0);
 while(true){
-if((i__12172 < size__4528__auto__)){
-var key = cljs.core._nth.call(null,c__4527__auto__,i__12172);
+if((i__10501 < size__4528__auto__)){
+var key = cljs.core._nth.call(null,c__4527__auto__,i__10501);
 if(((cljs.core.not.call(null,cljs.core.get.call(null,seen,key))) && (((cljs.core.empty_QMARK_.call(null,prefix)) || (clojure.string.starts_with_QMARK_.call(null,key,prefix)))))){
-cljs.core.chunk_append.call(null,b__12173,(function (){var prop = goog.object.get(props,key);
+cljs.core.chunk_append.call(null,b__10502,(function (){var prop = goog.object.get(props,key);
 cljs.core.conj_BANG_.call(null,seen,key);
 
 return new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"name","name",1843675177),key,new cljs.core.Keyword(null,"hierarchy","hierarchy",-1053470341),i,new cljs.core.Keyword(null,"type","type",1174270348),(function (){try{var temp__5751__auto__ = (function (){var or__4126__auto__ = goog.object.get(prop,"value");
@@ -103,22 +103,22 @@ return "var";
 } else {
 return "var";
 }
-}catch (e12180){if((e12180 instanceof Error)){
-var e = e12180;
+}catch (e10508){if((e10508 instanceof Error)){
+var e = e10508;
 return "var";
 } else {
-throw e12180;
+throw e10508;
 
 }
 }})()], null);
 })());
 
-var G__12188 = (i__12172 + (1));
-i__12172 = G__12188;
+var G__10511 = (i__10501 + (1));
+i__10501 = G__10511;
 continue;
 } else {
-var G__12189 = (i__12172 + (1));
-i__12172 = G__12189;
+var G__10512 = (i__10501 + (1));
+i__10501 = G__10512;
 continue;
 }
 } else {
@@ -127,12 +127,12 @@ return true;
 break;
 }
 })()){
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__12173),cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__12168_$_iter__12170.call(null,cljs.core.chunk_rest.call(null,s__12171__$2)));
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__10502),cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__10497_$_iter__10499.call(null,cljs.core.chunk_rest.call(null,s__10500__$2)));
 } else {
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__12173),null);
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__10502),null);
 }
 } else {
-var key = cljs.core.first.call(null,s__12171__$2);
+var key = cljs.core.first.call(null,s__10500__$2);
 if(((cljs.core.not.call(null,cljs.core.get.call(null,seen,key))) && (((cljs.core.empty_QMARK_.call(null,prefix)) || (clojure.string.starts_with_QMARK_.call(null,key,prefix)))))){
 return cljs.core.cons.call(null,(function (){var prop = goog.object.get(props,key);
 cljs.core.conj_BANG_.call(null,seen,key);
@@ -154,18 +154,18 @@ return "var";
 } else {
 return "var";
 }
-}catch (e12182){if((e12182 instanceof Error)){
-var e = e12182;
+}catch (e10509){if((e10509 instanceof Error)){
+var e = e10509;
 return "var";
 } else {
-throw e12182;
+throw e10509;
 
 }
 }})()], null);
-})(),cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__12168_$_iter__12170.call(null,cljs.core.rest.call(null,s__12171__$2)));
+})(),cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__10497_$_iter__10499.call(null,cljs.core.rest.call(null,s__10500__$2)));
 } else {
-var G__12190 = cljs.core.rest.call(null,s__12171__$2);
-s__12171__$1 = G__12190;
+var G__10513 = cljs.core.rest.call(null,s__10500__$2);
+s__10500__$1 = G__10513;
 continue;
 }
 }
@@ -174,16 +174,16 @@ return null;
 }
 break;
 }
-});})(s__12169__$1,vec__12174,i,map__12177,map__12177__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen))
+});})(s__10498__$1,vec__10503,i,map__10506,map__10506__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen))
 ,null,null));
-});})(s__12169__$1,vec__12174,i,map__12177,map__12177__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen))
+});})(s__10498__$1,vec__10503,i,map__10506,map__10506__$1,obj,props,xs__6308__auto__,temp__5753__auto__,seen))
 ;
 var fs__4526__auto__ = cljs.core.seq.call(null,iterys__4525__auto__.call(null,cljs.core.js_keys.call(null,props)));
 if(fs__4526__auto__){
-return cljs.core.concat.call(null,fs__4526__auto__,cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__12168.call(null,cljs.core.rest.call(null,s__12169__$1)));
+return cljs.core.concat.call(null,fs__4526__auto__,cider$nrepl$inlined_deps$suitable$v0v4v0$suitable$js_introspection$iter__10497.call(null,cljs.core.rest.call(null,s__10498__$1)));
 } else {
-var G__12191 = cljs.core.rest.call(null,s__12169__$1);
-s__12169__$1 = G__12191;
+var G__10514 = cljs.core.rest.call(null,s__10498__$1);
+s__10498__$1 = G__10514;
 continue;
 }
 } else {
--- src/colorpie_renderer/sets.cljs
+++ src/colorpie_renderer/sets.cljs
@@ -1,10 +1,37 @@
-(ns colorpie-renderer.sets)
-(require '[colorpie-renderer.nodes :as nodes])
+(ns colorpie-renderer.sets
+  (:require [colorpie-renderer.nodes :as nodes])
+  (:require-macros [colorpie-renderer.sets-macros :as macros]))
 
 ;; This is what collects 4 intersections. It expressions multiple color union.
 (defrecord Intersection-sets [name nodes])
 
-(def basis [nodes/peace nodes/perfection nodes/power nodes/freedom nodes/acceptance])
-(def nodes [nodes/consideration nodes/method nodes/reason nodes/becoming nodes/exploitment
-            nodes/self-interest nodes/casualness nodes/frankness nodes/inherency nodes/harmony])
-(def intersection-nodes [])
+(def intersection-sets [(macros/def-sets promotion
+                                         map->Intersection-sets {:name "推進"
+                                                                 :nodes [nodes/construction nodes/recommendation nodes/conference nodes/training]})
+                        (macros/def-sets arbitrariness
+                                         map->Intersection-sets {:name "随意"
+                                                                 :nodes [nodes/manipulation nodes/desire nodes/calculation nodes/reformation]})
+                        (macros/def-sets individuality
+                                         map->Intersection-sets {:name "独立"
+                                                                 :nodes [nodes/unfetteredness nodes/craving nodes/challenge nodes/delight]})
+                        (macros/def-sets sensitiveness
+                                         map->Intersection-sets {:name "感性"
+                                                                 :nodes [nodes/feelings nodes/emergence nodes/throbbing nodes/emphathize]})
+                        (macros/def-sets continuity
+                                         map->Intersection-sets {:name "連綿"
+                                                                 :nodes [nodes/affection nodes/healthness nodes/dedication nodes/derivation]})
+                        (macros/def-sets convinience
+                                         map->Intersection-sets {:name "利便"
+                                                                 :nodes [nodes/efficiency nodes/credit nodes/supply nodes/expediency]})
+                        (macros/def-sets creation
+                                         map->Intersection-sets {:name "創造"
+                                                                 :nodes [nodes/ingenuity nodes/inspiration nodes/experiment nodes/inquiry]})
+                        (macros/def-sets outliving
+                                         map->Intersection-sets {:name "残存"
+                                                                 :nodes [nodes/sturdiness nodes/territory nodes/overwhelming nodes/preservation]})
+                        (macros/def-sets enforcement
+                                         map->Intersection-sets {:name "励行"
+                                                                 :nodes [nodes/first-aid nodes/occasion nodes/bravery nodes/swift]})
+                        (macros/def-sets clinical
+                                         map->Intersection-sets {:name "臨床"
+                                                                 :nodes [nodes/observation nodes/formation nodes/prudence nodes/adaption]})])
