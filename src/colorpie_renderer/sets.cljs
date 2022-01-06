79942ee1a937c1b3cea3545a398c02cd703d92f0
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-06 15:37:10 +0900

    reindent

--- src/colorpie_renderer/sets.cljs
+++ src/colorpie_renderer/sets.cljs
@@ -7,32 +7,32 @@
 
 (def intersection-sets
   [(macros/def-sets promotion
-                    map->Intersection-sets {:name "推進"
-                                            :nodes [nodes/construction nodes/recommendation nodes/conference nodes/training]})
+     map->Intersection-sets {:name "推進"
+                             :nodes [nodes/construction nodes/recommendation nodes/conference nodes/training]})
    (macros/def-sets arbitrariness
-                    map->Intersection-sets {:name "随意"
-                                            :nodes [nodes/manipulation nodes/desire nodes/calculation nodes/reformation]})
+     map->Intersection-sets {:name "随意"
+                             :nodes [nodes/manipulation nodes/desire nodes/calculation nodes/reformation]})
    (macros/def-sets individuality
-                    map->Intersection-sets {:name "独立"
-                                            :nodes [nodes/unfetteredness nodes/craving nodes/challenge nodes/delight]})
+     map->Intersection-sets {:name "独立"
+                             :nodes [nodes/unfetteredness nodes/craving nodes/challenge nodes/delight]})
    (macros/def-sets sensitiveness
-                    map->Intersection-sets {:name "感性"
-                                            :nodes [nodes/feelings nodes/emergence nodes/throbbing nodes/emphathize]})
+     map->Intersection-sets {:name "感性"
+                             :nodes [nodes/feelings nodes/emergence nodes/throbbing nodes/emphathize]})
    (macros/def-sets continuity
-                    map->Intersection-sets {:name "連綿"
-                                            :nodes [nodes/affection nodes/healthness nodes/dedication nodes/derivation]})
+     map->Intersection-sets {:name "連綿"
+                             :nodes [nodes/affection nodes/healthness nodes/dedication nodes/derivation]})
    (macros/def-sets convinience
-                    map->Intersection-sets {:name "利便"
-                                            :nodes [nodes/efficiency nodes/credit nodes/supply nodes/expediency]})
+     map->Intersection-sets {:name "利便"
+                             :nodes [nodes/efficiency nodes/credit nodes/supply nodes/expediency]})
    (macros/def-sets creation
-                    map->Intersection-sets {:name "創造"
-                                            :nodes [nodes/ingenuity nodes/inspiration nodes/experiment nodes/inquiry]})
+     map->Intersection-sets {:name "創造"
+                             :nodes [nodes/ingenuity nodes/inspiration nodes/experiment nodes/inquiry]})
    (macros/def-sets outliving
-                    map->Intersection-sets {:name "残存"
-                                            :nodes [nodes/sturdiness nodes/territory nodes/overwhelming nodes/preservation]})
+     map->Intersection-sets {:name "残存"
+                             :nodes [nodes/sturdiness nodes/territory nodes/overwhelming nodes/preservation]})
    (macros/def-sets enforcement
-                    map->Intersection-sets {:name "励行"
-                                            :nodes [nodes/first-aid nodes/occasion nodes/bravery nodes/swift]})
+     map->Intersection-sets {:name "励行"
+                             :nodes [nodes/first-aid nodes/occasion nodes/bravery nodes/swift]})
    (macros/def-sets clinical
-                    map->Intersection-sets {:name "臨床"
-                                            :nodes [nodes/observation nodes/formation nodes/prudence nodes/adaption]})])
+     map->Intersection-sets {:name "臨床"
+                             :nodes [nodes/observation nodes/formation nodes/prudence nodes/adaption]})])
