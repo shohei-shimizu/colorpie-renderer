343bea4790d74669aebacd1ed13607c166a59306
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2022-01-17 11:31:37 +0900

    added requires and create elements

--- src/colorpie_renderer/core.cljs
+++ src/colorpie_renderer/core.cljs
@@ -1,6 +1,9 @@
 (ns colorpie-renderer.core
   (:require [cljsjs.jquery :as jquery]
-            [cljsjs.d3 :as d3]))
+            [cljsjs.d3 :as d3]
+            [colorpie-renderer.nodes :as nodes]
+            [colorpie-renderer.sets :as sets])
+  (:require-macros [colorpie-renderer.macros.core :as macros]))
 
 (def canvas (.getElementById js/document "canvas"))
 (def ctx (.getContext canvas "2d"))
@@ -9,3 +12,9 @@
 (.beginPath ctx)
 (.rect ctx 100 100 50 30)
 (.fill ctx)
+
+(doseq [n nodes/nodes]
+  (-> js/d3
+      (.select "body")
+      (.append "div")
+      (.attr "id")))
---
+++ src/colorpie_renderer/macros/core.clj
@@ -0,0 +1,1 @@
+(ns colorpie-renderer.macros.core)
